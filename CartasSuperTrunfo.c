cade8c82f68d3f53cdbf697715ea4c79626bfa0c
clay1605 <clay1605@hotmail.com>
2025-09-15 12:10:45 +0000

    Versão 1 CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,16 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+  printf(Desafio Supertrunfo!\n);
+  
   // Área para definição das variáveis para armazenar as propriedades das cidades
+char Estado[50];
+char Códigocarta[50];
+char Nomecidade[50];
+int População;
+float Áreaemkm2;
+float PIB;
+int Pontos túristicos;
 
   // Área para entrada de dados
 
